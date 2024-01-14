b93208e1e4063e8fae7b467fc6eeb1b2b8ddbeda
JankoLoL <adam.chch86@gmail.com>
2024-01-14 09:58:02 +0100

    proper formset for ingredients with button

--- eat_fit_app/forms.py
+++ eat_fit_app/forms.py
@@ -44,32 +44,3 @@ class RecipeIngredientForm(forms.ModelForm):
 
 
 RecipeIngredientFormSet = inlineformset_factory(Recipe, RecipeIngredients, form=RecipeIngredientForm, extra=1)
-
-#
-# class LoginForm(forms.Form):
-#     username = forms.CharField()
-#     password = forms.CharField(widget=forms.PasswordInput)
-#
-#     def clean(self):
-#         cleaned_data = super().clean()
-#
-#         user = authenticate(**cleaned_data)
-#         if user is None:
-#             raise ValidationError('Incorrect login or password')
-#         cleaned_data['user'] = user
-#         return cleaned_data
-#
-#
-# class UserCreateForm(forms.ModelForm):
-#     password1 = forms.CharField(widget=forms.PasswordInput, label='password1')
-#     password2 = forms.CharField(widget=forms.PasswordInput, label='password2')
-#
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         if cleaned_data['password1'] != cleaned_data['password2']:
-#             raise ValidationError('Passwords are not equal')
-#         return cleaned_data
-#
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email']
--- eat_fit_app/static/js/add_ingredient.js
+++ eat_fit_app/static/js/add_ingredient.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.getElementById('add-more');
     const formSetContainer = document.getElementById('form-set');
@@ -10,12 +8,23 @@ document.addEventListener("DOMContentLoaded", function () {
             let formNum = parseInt(totalForms.value); // Get the current number of forms
             let newForm = formSetContainer.children[0].cloneNode(true); // Clone the first form
 
-            newForm.innerHTML = newForm.innerHTML.replace(/-0-/g, `-${formNum}-`); // Replace the form number
+            newForm.innerHTML = newForm.innerHTML.replace(/-\d-/g, `-${formNum}-`); // Replace the form number
             formSetContainer.appendChild(newForm);
 
             totalForms.value = formNum + 1; // Increment the form count
         } else {
             console.error("Total forms input not found");
+        }
+    });
+
+    formSetContainer.addEventListener('click', function (e) {
+        if (e.target && e.target.classList.contains('remove-ingredient')) {
+            e.preventDefault();
+            e.target.closest('.ingredient-form').remove();
+
+            // Update the total forms count
+            let totalForms = document.querySelector("input[name='recipeingredients_set-TOTAL_FORMS']");
+            totalForms.value = parseInt(totalForms.value) - 1;
         }
     });
 });
