2d31e407723db01ae4793628f1ab487cc30a9478
JankoLoL <adam.chch86@gmail.com>
2024-01-14 10:53:03 +0100

    edit profile message and redirect

--- user/views.py
+++ user/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 from django.views.generic import CreateView, UpdateView
 from .forms import EditUserProfileForm
 
@@ -33,11 +34,12 @@ class UserEditView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = EditUserProfileForm
     template_name = 'user/edit-profile.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('user:edit-profile')
 
     def get_object(self):
         return self.request.user
 
     def form_valid(self, form):
-        form.save()
-        return redirect('index')
+        super(UserEditView, self).form_valid(form)
+        messages.success(self.request, 'Profile successfully updated')
+        return redirect('user:edit-profile')
