a41298914daf8a324532fafb194bc20bd2467035
JankoLoL <adam.chch86@gmail.com>
2024-02-23 04:08:22 +0100

    changed to BaseTest and next tests for model

--- eat_fit_app/tests/test_models.py
+++ eat_fit_app/tests/test_models.py
@@ -1,31 +1,45 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from eat_fit_app.models import Recipe, Ingredients, RecipeIngredients, Category, RecipeIngredientsMeasure, \
-    RecipeCategory, Occasion, Cuisine
+    RecipeCategory, Occasion, Cuisine, RecipeOccasion, RecipeCuisine
 
 
-class RecipeModelTest(TestCase):
-
+class BaseTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user(username='testuser', password='12345')
+
+        cls.ingredient_tomato = Ingredients.objects.create(name='Tomato')
+        cls.ingredient_sugar = Ingredients.objects.create(name='Sugar')
+
         cls.recipe = Recipe.objects.create(
             name='Test Recipe',
             description='Test Description',
             instructions='Test Instructions',
             user=cls.user
         )
-        cls.ingredient = Ingredients.objects.create(name='Tomato')
-        RecipeIngredients.objects.create(recipe=cls.recipe, ingredients=cls.ingredient, quantity=3)
+
+        cls.recipe_ingredient = RecipeIngredients.objects.create(
+            recipe=cls.recipe,
+            ingredients=cls.ingredient_tomato,
+            quantity=3,
+            measure=RecipeIngredientsMeasure.objects.create(measure='Cups')
+        )
+
+        cls.category = Category.objects.create(name='Dessert', description='Sweet food')
+
+        cls.occasion = Occasion.objects.create(name='Birthday', description='Birthday party food')
+
+        cls.cuisine = Cuisine.objects.create(name='Italian', description='Italian cuisine')
+
+        RecipeCategory.objects.create(recipe=cls.recipe, category=cls.category)
+        RecipeOccasion.objects.create(recipe=cls.recipe, occasion=cls.occasion)
 
 
-
-
+class RecipeModelTest(BaseTest):
     def test_recipe_creation(self):
         self.assertTrue(isinstance(self.recipe, Recipe))
-        self.assertEqual(self.recipe.__str__(), 'Test Recipe')
-        self.assertEqual(self.recipe.description, 'Test Description')
-        self.assertEqual(self.recipe.instructions, 'Test Instructions')
+        self.assertEqual(str(self.recipe), 'Test Recipe')
 
     def test_recipe_deletion(self):
         self.assertEqual(RecipeIngredients.objects.count(), 1)
@@ -36,87 +50,67 @@ class RecipeModelTest(TestCase):
         self.assertEqual(self.recipe.user, self.user)
 
     def test_recipe_fields(self):
-        recipe = Recipe.objects.get(id=self.recipe.id)
-        self.assertEqual(recipe.name, 'Test Recipe')
-        self.assertEqual(recipe.description, 'Test Description')
-        self.assertEqual(recipe.instructions, 'Test Instructions')
+        self.assertEqual(self.recipe.name, 'Test Recipe')
+        self.assertEqual(self.recipe.description, 'Test Description')
 
     def test_recipe_update(self):
-        recipe = Recipe.objects.get(id=self.recipe.id)
-        recipe.name = 'Updated Name'
-        recipe.save()
-        self.assertEqual(recipe.name, 'Updated Name')
+        self.recipe.name = 'Updated Name'
+        self.recipe.save()
+        self.assertEqual(self.recipe.name, 'Updated Name')
 
 
-class CategoryModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.category = Category.objects.create(name='Dessert', description='Sweet food')
-
+class CategoryModelTest(BaseTest):
     def test_category_creation(self):
         self.assertTrue(isinstance(self.category, Category))
-        self.assertEqual(self.category.__str__(), 'Dessert')
-        self.assertEqual(self.category.description, 'Sweet food')
+        self.assertEqual(str(self.category), 'Dessert')
 
 
-class IngredientsModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.ingredient = Ingredients.objects.create(name='Sugar')
-
+class IngredientsModelTest(BaseTest):
     def test_ingredient_creation(self):
-        self.assertTrue(isinstance(self.ingredient, Ingredients))
-        self.assertEqual(self.ingredient.__str__(), 'Sugar')
+        self.assertTrue(isinstance(self.ingredient_tomato, Ingredients))
+        self.assertEqual(str(self.ingredient_tomato), 'Tomato')
 
 
-class RecipeIngredientsModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        user = User.objects.create_user(username='testuser2', password='12345')
-        recipe = Recipe.objects.create(name='Cake', description='Delicious cake', user=user)
-        ingredient = Ingredients.objects.create(name='Flour')
-        measure = RecipeIngredientsMeasure.objects.create(measure='Cups')
-        cls.recipe_ingredient = RecipeIngredients.objects.create(
-            recipe=recipe,
-            ingredients=ingredient,
-            quantity=2,
-            measure=measure
-        )
-
+class RecipeIngredientsModelTest(BaseTest):
     def test_recipe_ingredient_creation(self):
         self.assertTrue(isinstance(self.recipe_ingredient, RecipeIngredients))
-        self.assertEqual(self.recipe_ingredient.quantity, 2)
+        self.assertEqual(self.recipe_ingredient.quantity, 3)
         self.assertEqual(self.recipe_ingredient.measure.measure, 'Cups')
 
 
-class RecipeCategoryRelationTest(TestCase):
-    def setUp(self):
-        self.recipe = Recipe.objects.create(name='Salad', description='Fresh salad')
-        self.category = Category.objects.create(name='Healthy', description='Healthy food')
-
+class RecipeCategoryRelationTest(BaseTest):
     def test_recipe_category_relation(self):
-        RecipeCategory.objects.create(recipe=self.recipe, category=self.category)
-        self.assertEqual(self.recipe.categories.first().name, 'Healthy')
-        self.assertEqual(self.category.recipes.first().name, 'Salad')
+        self.assertIn(self.category, self.recipe.categories.all())
 
 
-class OccasionModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.occasion = Occasion.objects.create(name='Birthday', description='Birthday party food')
-
+class OccasionModelTest(BaseTest):
     def test_occasion_creation(self):
         self.assertTrue(isinstance(self.occasion, Occasion))
-        self.assertEqual(self.occasion.__str__(), 'Birthday')
-        self.assertEqual(self.occasion.description, 'Birthday party food')
+        self.assertEqual(str(self.occasion), 'Birthday')
 
 
-class CuisineModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.cuisine = Cuisine.objects.create(name='Italian', description='Italian cuisine')
-
+class CuisineModelTest(BaseTest):
     def test_cuisine_creation(self):
         self.assertTrue(isinstance(self.cuisine, Cuisine))
-        self.assertEqual(self.cuisine.__str__(), 'Italian')
-        self.assertEqual(self.cuisine.description, 'Italian cuisine')
+        self.assertEqual(str(self.cuisine), 'Italian')
+
+
+# Additional Tests
+class RecipeOccasionRelationTest(BaseTest):
+    def test_recipe_occasion_relation(self):
+        self.assertIn(self.occasion, self.recipe.occasions.all())
+
+
+class RecipeCuisineRelationTest(BaseTest):
+    def setUp(self):
+        self.new_recipe = Recipe.objects.create(
+            name='Pizza',
+            description='Italian pizza',
+            instructions='Bake for 20 minutes',
+            user=self.user
+        )
+        RecipeCuisine.objects.create(recipe=self.new_recipe, cuisine=self.cuisine)
+
+    def test_recipe_cuisine_relation(self):
+        self.assertIn(self.cuisine, self.new_recipe.cuisines.all())
+        self.assertIn(self.new_recipe, self.cuisine.recipes.all())
