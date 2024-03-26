fe19331f0794dffa749429fbed711881d158aa5f
JankoLoL <adam.chch86@gmail.com>
2024-03-26 09:27:40 +0100

    db changed to local no docker required

--- eat_fit_django_project/settings.py
+++ eat_fit_django_project/settings.py
@@ -30,7 +30,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='db'),
+        'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', cast=int),
     }
 }
